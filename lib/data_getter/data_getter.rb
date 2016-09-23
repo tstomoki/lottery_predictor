021249b646528112655cc4df9f3423a77b0bb473
tstomoki <tstomoki4@gmail.com>
2016-09-23 20:26:59 +0900

    implemented 1 year getter

--- lib/data_getter/data_getter.rb
+++ lib/data_getter/data_getter.rb
@@ -8,20 +8,36 @@ module DataGetter
     require 'open-uri'
     require 'date'
     require 'pry'
+    require 'pry-byebug'
 
     HEADER = ['Date', 'Location', 'Round',
-              'Home', 'Away', 'Home Point', 'Away Point', 'Lottery Score']
+              'Home', 'Score', 'Away', 'Result']
     USER_AGENT = "User-Agent: Mozilla/5.0 (Windows NT 6.1; rv:28.0) Gecko/20100101 Firefox/28.0"
     REFERER = "http://google.com"
     def obtain_detail_data(round_str, uri)
+      ret_array = []
       open(uri, 'r:Shift_JIS',
            "User-Agent" => USER_AGENT,
            "Referer" => REFERER) do |data|
         sleep(1.0)        
         doc = Nokogiri::HTML(data)
-        main_table = doc.xpath('//*[@id="mainarea"]/div[2]/div/table[1]')
-        binding.pry
+        main_table = doc.xpath('//*[@id="mainarea"]/div[2]/div/table[1]')[0]
+        main_table.xpath('tr').each do |tr|
+          game_info = tr.css('td')
+          next if game_info.empty?
+          insert_data = game_info.map{|gi| gi.content.strip}
+          # block for less data
+          next if HEADER.size != insert_data.size
+          begin
+            tmp_hash = [HEADER.map(&:to_sym), insert_data].transpose
+            tmp_hash = Hash[*tmp_hash.flatten]
+          rescue => e
+            binding.pry
+          end
+          ret_array.push tmp_hash
+        end
       end
+      ret_array
     end
     
     def obtain_whole_data
@@ -40,7 +56,12 @@ module DataGetter
           round_info.map do |ri|
             round_str = ri.content
             round_uri = [base_uri, ri.attributes['href'].value].join
-            obtain_detail_data(round_str, round_uri)
+            round_uri.match(/CntId=(\d+)/)
+            round = $1.to_i
+            p round
+            detail_data = obtain_detail_data(round_str, round_uri)
+            detail_data.map{|dd| dd[:Round] = round.to_i}
+            ret_data.push detail_data
           end
         end
       end    
